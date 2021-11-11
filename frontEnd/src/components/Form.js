2883398bff5a5c311c84332bf178c52fe21568a3
Eri <efwinckler@gmail.com>
2021-11-11 00:24:19 -0500

    working on processing the form

--- frontEnd/src/components/Form.js
+++ frontEnd/src/components/Form.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 //department
 //status
@@ -6,27 +7,37 @@ import React from "react";
 //title
 //content
 function Form() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <section id="form">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h5>Department</h5>
-        <input
-          type="radio"
-          name="department"
-          value="general"
-          id="general"
-          required
-        />
-        <label for="general">General</label>
-        <input type="radio" name="department" value="frontend" id="frontend" />
-        <label for="frontend">Front End</label>
-        <input type="radio" name="department" value="backend" id="backend" />
-        <label for="backend">Back End</label>
 
+        <select {...register("department")}>
+          <option value="general">General</option>
+          <option value="frontend">Front End</option>
+          <option value="backend">Back End</option>
+        </select>
+        {
+          // <input
+          //   type="radio"
+          //   name="department"
+          //   value="general"
+          //   id="general"
+          //   required
+          // />
+          // <label for="general">General</label>
+          // <input type="radio" name="department" value="frontend" id="frontend" />
+          // <label for="frontend">Front End</label>
+          // <input type="radio" name="department" value="backend" id="backend" />
+          // <label for="backend">Back End</label>
+        }
         <h5>
           <label for="status">Status:</label>
         </h5>
-        <select name="status" id="status">
+        <select name="status" id="status" {...register("status")}>
           <option value="active">Active</option>
           <option value="inactive">Inactive</option>
         </select>
@@ -34,16 +45,27 @@ function Form() {
         <h5>
           <label for="title">Title</label>
         </h5>
-        <input type="text" id="title" name="title" />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          {...register("title", { required: true })}
+        />
 
         <h5>
           <label for="content">Content</label>
         </h5>
-        <textarea id="w3review" name="w3review" rows="4" cols="50">
+        <textarea
+          id="w3review"
+          name="w3review"
+          rows="4"
+          cols="50"
+          {...register("content", { required: true })}
+        >
           Please specify the issue.
         </textarea>
         <br />
-        <input type="submit" value="Submit"></input>
+        <input type="submit" value="Submit" />
       </form>
     </section>
   );
